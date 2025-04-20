3768819f7fad42bfe0496b7842ccfc2f9039b392
Chaire-H <claire.hshin619@gmail.com>
2025-04-20 11:07:56 +0800

    update

--- db_payment.py
+++ db_payment.py
@@ -9,32 +9,30 @@ def connect_db():
     )
 print("Database ready!") 
 
-# 正式訂單
+# 付款紀錄
 def create_table():
     db_connect = connect_db()
     cursor = db_connect.cursor()
     create_table = """
-        CREATE TABLE tp_order (
+        CREATE TABLE tp_payment (
             id INT PRIMARY KEY AUTO_INCREMENT, 
-            member_id INT,
             order_number VARCHAR(255) UNIQUE,
-            order_price INT,
-            paymentStatus VARCHAR(255) CHECK (paymentStatus IN ("PAID", "UNPAID")),
-            attractionId INT,
-            tourDate DATE,
-            dateTime VARCHAR(255),
-            contact_name VARCHAR(255),
-            contact_email VARCHAR(255),
-            contact_phone VARCHAR(255),
+            status INT,
+            msg TEXT,
+            currency VARCHAR(255), 
+            amount INT,
+            rec_trade_id VARCHAR(255),
+            bank_transaction_id VARCHAR(255),
+            paid_at DATETIME DEFAULT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            paid_at TIMESTAMP DEFAULT NULL
+            FOREIGN KEY (order_number) REFERENCES tp_order(order_number)
         );
     """
     cursor.execute(create_table)
     db_connect.commit()
     cursor.close()
     db_connect.close()
-    print("資料表 'tp_order' 建立成功！")
+    print("資料表 'tp_payment' 建立成功！")
 
 # 執行程式
 create_table()
